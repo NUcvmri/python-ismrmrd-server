a3ba0abf73e82d71f2282ef9fa0578af60451e5f
hberhane <hberhane@northwestern.edu>
2020-08-20 00:17:04 -0500

    adjusted the .sh files with correct path

--- sync-code-and-start-fire-python-server.sh
+++ sync-code-and-start-fire-python-server.sh
@@ -3,12 +3,12 @@
 # Bash script to start Python ISMRMRD server.  First argument is path to log file.
 # If no argument is provided, logging is done to stdout (and discarded)
 
-cp -R -f /tmp/share/code/* "/opt/code/python-ismrmrd-server/"
+#cp -R -f /tmp/share/code/* "/opt/code/python-ismrmrd-server/"
 
 if [ $# -eq 1 ]; then
   LOG_FILE=${1}
-  /usr/local/bin/python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 -l=${LOG_FILE} &
+  /usr/local/bin/python /opt/codes/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 -l=${LOG_FILE} &
 else
-  /usr/local/bin/python3 /opt/code/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 &
+  /usr/local/bin/python /opt/codes/python-ismrmrd-server/main.py -v -H=0.0.0.0 -p=9002 &
 fi
 
